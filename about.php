afdf1354a1fa65ae127448b15e2133043161909f
Neha Adhikari <nehaadhikari2005@gmail.com>
2025-06-20 13:19:52 +0530

    about.php

--- about.php
+++ about.php
@@ -4,7 +4,7 @@
     <title></title>
     <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <link rel="stylesheet" type="text/css" href="style.css">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
   <link href="https://fonts.googleapis.com/css?family=Josefin+Sans&display=swap" rel="stylesheet">
 </head>
@@ -21,7 +21,7 @@
     <div class="container-fluid">
     <div class="row">
         <div class="col-lg-6 col-md-6 col-12">
-            <img src="images/107111100.webp" class="img-fluid aboutimg">
+            <img src="107111100.webp" class="img-fluid aboutimg">
         </div>
         <div class="col-lg-6 col-md-6 col-12">
             <h2 class="display-4"> Welcome to Delhi </h2>
